c9ffcbdd4d2f479f5e5e8b70a3d9e02e994dcc05
Artem Pavlov <art.pavlov.2012@gmail.com>
2023-04-20 11:00:33 +0300

    add random password generation

--- start.sh
+++ start.sh
@@ -5,4 +5,9 @@ chown -R 33:33 ./db
 chown -R 33:33 ./certs
 chown -R 33:33 ./src
 chown -R 33:33 ./nginx-configs
+IP=$(curl https://ipinfo.io/ip)
+PASS=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 12 ; echo '')
+/bin/cp -rf ./src/conf.php.template ./src/www/conf.php
+sed -i "s/_PASSWORD_/$PASS/g" ./src/www/conf.php
 docker compose up -d --build
+echo "Login to\nhttps://$IP\nwith password:\n$PASS"
