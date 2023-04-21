c90c5ace65e7c0aa133dfdd5c7a054aee0efb1b3
General M <51231797+momentum100@users.noreply.github.com>
2023-04-21 21:05:06 +0300

    Update index.php

--- src/www/index.php
+++ src/www/index.php
@@ -28,7 +28,7 @@ if (isset($_GET['err'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nginx Configuration</title>
+    <title>Reverse proxy UI</title>
     <style>
         body {
             padding: 100px 0;
